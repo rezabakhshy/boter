91b897df033299f4fc5f8b1bffbf6ca1c4997186
rezabakhshy <87357982+rezabakhshy@users.noreply.github.com>
2021-11-26 13:52:32 +0330

    Update bot.py

--- bot.py
+++ bot.py
@@ -1,4 +1,5 @@
 from pyrogram import Client,filters
+from random import randint
 from gtts import gTTS
 import os
 app = Client("my_accound",api_id=12721742,api_hash="2a81674bd5e1ccbaed8c07f898d614ca")
@@ -11,17 +12,17 @@ def media(client, message):
     if text2=="!youvid":
         chat_id=message.chat.id
         bot_results = app.get_inline_bot_results("vid",text)
-        app.send_inline_bot_result(chat_id, bot_results.query_id, bot_results.results[0].id)
+        app.send_inline_bot_result(chat_id, bot_results.query_id, bot_results.results[randint(0,30)].id)
 
     elif text2=="!youpic":
         chat_id=message.chat.id
         bot_results = app.get_inline_bot_results("pic",text)
-        app.send_inline_bot_result(chat_id, bot_results.query_id, bot_results.results[0].id)
+        app.send_inline_bot_result(chat_id, bot_results.query_id, bot_results.results[randint(0,30)].id)
 
     elif text2=="!yougif":
         chat_id=message.chat.id
         bot_results = app.get_inline_bot_results("gif",text)
-        app.send_inline_bot_result(chat_id, bot_results.query_id, bot_results.results[0].id)
+        app.send_inline_bot_result(chat_id, bot_results.query_id, bot_results.results[randint(0,30)].id)
 
     elif text2=="!ttr":
         chat_id=message.chat.id
