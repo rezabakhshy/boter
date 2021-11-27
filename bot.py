522891c3a7453578ec811cfa318b1c46fa19b532
rezabakhshy <87357982+rezabakhshy@users.noreply.github.com>
2021-11-27 19:40:49 +0330

    Update bot.py

--- bot.py
+++ bot.py
@@ -33,16 +33,6 @@ def media(client, message):
         myobj.save("test.ogg")
         client.send_audio(chat_id,"test.ogg",reply_to_message_id=message_id)
         os.remove('test.ogg')
-
-    elif text2=="!tts":
-        chat_id=message.chat.id
-        message_id=message.message_id
-        text = message.reply_to_message.text
-        language="en"
-        myobj=gTTS(text=text,lang=language,slow=False)
-        myobj.save("test.ogg")
-        client.send_audio(chat_id,"test.ogg",reply_to_message_id=message_id)
-        os.remove('test.ogg')
        
     elif text2=="!help":
         help="**command:**\n!youvid\n**descriptin:**\nsearch name video and sending link\n\n\n**command:**\n!youpic\n**descriptin:**\nsearch picture with name and sending picture\n\n\n**command:**\n!yougif\n**descriptin:**\nsearch gif with name and sending gif\n\n\n**command:**\n!ttr\n**descriptin:**\nget text and change to voice and sending voice\n\n\n**command:**\n!tts\n**descriptin:**\nget message reply text and change to voice and sending voice\n\n\n"
