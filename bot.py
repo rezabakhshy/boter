6a9a2e45f7dc2ceab917e59a5420810f715d7974
rezabakhshy <87357982+rezabakhshy@users.noreply.github.com>
2021-12-04 12:36:35 +0330

    Update bot.py

--- bot.py
+++ bot.py
@@ -26,7 +26,6 @@ def logo2(client,message):
     chat_id=message.chat.id
     name=text.replace("!logo2 ","")
     num=randint(58,109)
-    print(num)
     Response=requests.post(f"https://api.codebazan.ir/ephoto/writeText?output=image&effect=create-online-black-and-white-layerlogo-{num}.html&text={name}")
     with open("logo2.jpg","wb") as f:
         f.write(Response.content)   
@@ -152,9 +151,106 @@ def ttr(client,message):
     client.send_audio(chat_id,"testvoice.ogg",reply_to_message_id=message.message_id)
     os.remove('testvoice.ogg')
 
+@app.on_message((filters.me) & filters.regex("^!bio$"))
+def biografi(client,message):
+    Response=requests.post("https://api.codebazan.ir/bio")
+    text=Response.text
+    client.edit_message_text(chat_id=message.chat.id,message_id=message.message_id,text=text)
+
+@app.on_message((filters.me) & filters.regex("^!newyear$"))
+def newyear(client,message):
+    response=requests.post("https://api.codebazan.ir/new-year")
+    tex=response.json()
+    text=""
+    day=tex["day"]
+    text+=f"{day} روز و"
+    hour=tex["hour"]
+    text+=f"{hour} ساعت و"
+    min=tex["min"]
+    text+=f"{min} دقیقه و"
+    sec=tex["sec"]
+    text+=f"{sec} ثانیه دیگر تا نوروز مانده است."
+    client.edit_message_text(chat_id=message.chat.id,message_id=message.message_id,text=text)
+
+@app.on_message((filters.me) & filters.regex("^!saadi$"))
+def ghazalsaadi(client,message):
+    response=requests.post(f"https://api.codebazan.ir/ghazalsaadi/?type=json&id={randint(0,637)}")
+    tex=response.json()
+    title=tex["title"]
+    cont=tex["contents"]
+    text=f"**عنوان: ** `{title}`\n**غزل: **`{cont}`"
+    client.edit_message_text(chat_id=message.chat.id,message_id=message.message_id,text=text)
+
+@app.on_message((filters.me) & filters.regex("^!gif "))
+def gif(client,message):
+    text=message.text
+    chat_id=message.chat.id
+    messag_id=message.message_id
+    name=text.replace("!gif ","")
+    response=requests.post(f"https://api.codebazan.ir/image/?type=gif&text={name}")
+    text1=response.json()
+    url=text1[f"giflink{randint(1,11)}"]
+    gif=requests.get(url)
+    with open(f"{name}.gif","wb") as f:
+        f.write(gif.content)
+    client.send_animation(chat_id,f"{name}.gif",reply_to_message_id=messag_id)
+    os.remove(f"{name}.gif")
+
+@app.on_message((filters.me) & filters.regex("^!card$"))
+def visacard(client,message):
+    response=requests.post(f"https://api.codebazan.ir/visa-card/")
+    text=response.json()
+    i=randint(0,11)
+    name=text["Result"][i]["name"]
+    lastname=text["Result"][i]["lastname"]
+    adress=text["Result"][i]["Address"]
+    city=text["Result"][i]["City"]
+    state=text["Result"][i]["State"]
+    post=text["Result"][i]["Postalcode"]
+    country=text["Result"][i]["Country"]
+    birthday=text["Result"][i]["birthday"]
+    cardtype=text["Result"][i]["cardtype"]
+    number=text["Result"][i]["cardnumber"]
+    cvv2=text["Result"][i]["CVV2"]
+    expire=text["Result"][i]["Expirationdate"]
+    result=f"**name:** `{name}`\n**lastname:** `{lastname}`\n**address:** `{adress}`\n**city:** `{city}`\n**state:** `{state}`\n**postalcode:** `{post}`\n**country:** `{country}`\n**birthday:** `{birthday}`\n**cardtype:** `{cardtype}`\n**cardnumber:** `{number}`\n**cvv2:** `{cvv2}`\n**Expirationdate:** `{expire}`"
+    client.edit_message_text(chat_id=message.chat.id,message_id=message.message_id,text=result)
+
+
+@app.on_message((filters.me) & filters.regex("^!meli "))
+def meli(client,message):
+    text=message.text
+    code=text.replace("!meli ","")
+    Response=requests.post(f"https://api.codebazan.ir/codemelli/?code={code}")
+    tex=Response.json()
+    client.edit_message_text(chat_id=message.chat.id,message_id=message.message_id,text=tex["Result"])
+
+@app.on_message((filters.me) & filters.regex("^!air "))
+def air(client,message):
+    text=message.text
+    city=text.replace("!air ","")
+    Response=requests.post(f"https://api.codebazan.ir/weather/?city={city}")
+    tex=Response.json()
+    ostan=tex["result"]["استان"]
+    shahr=tex["result"]["شهر"]
+    dama=tex["result"]["دما"]
+    sorat=tex["result"]["سرعت باد"]
+    vaziat=tex["result"]["وضعیت هوا"]
+    fdama=tex["فردا"]["دما"]
+    fvaziat=tex["فردا"]["وضعیت هوا"]
+    text=f"**استان: ** {ostan}\n**شهر: ** {shahr}\n**          امروز **\n**دما: ** {dama}\n**سرعت باد: ** {sorat}\n**وضعیت هوا: ** {vaziat}\n\n      **فردا **\n**دما: ** {fdama}\n**وضعیت هوا: ** {fvaziat}"
+    client.edit_message_text(chat_id=message.chat.id,message_id=message.message_id,text=text)
+
 @app.on_message((filters.me) & filters.regex("^!help$"))
 def help(client,message):
     help=""
+    help+="**command:**\n!air\n**descriptin:**\nget city and send climatic condition\n\n/*/*/*/*/*/*/*/*/*/*/*/*/\n\n"
+    help+="**command:**\n!meli\n**descriptin:**\nsend result sending code meli\n\n/*/*/*/*/*/*/*/*/*/*/*/*/\n\n"
+    help+="**command:**\n!card\n**descriptin:**\nsend credit card\n\n/*/*/*/*/*/*/*/*/*/*/*/*/\n\n"
+    help+="**command:**\n!gif\n**descriptin:**\nget string and send gif withe string\n\n/*/*/*/*/*/*/*/*/*/*/*/*/\n\n"
+    help+="**command:**\n!saadi\n**descriptin:**\nsend one lyric from saadi\n\n/*/*/*/*/*/*/*/*/*/*/*/*/\n\n"
+    help+="**command:**\n!newyear\n**descriptin:**\nsend remaining amount until nowruz\n\n/*/*/*/*/*/*/*/*/*/*/*/*/\n\n"
+    help+="**command:**\n!bio\n**descriptin:**\nsend one bio\n\n/*/*/*/*/*/*/*/*/*/*/*/*/\n\n"
     help+="**command:**\n!vazhe\n**descriptin:**\nget word prsion and send meaning\n\n/*/*/*/*/*/*/*/*/*/*/*/*/\n\n"
     help+="**command:**\n!num\n**descriptin:**\nget number and send number to persion\n\n/*/*/*/*/*/*/*/*/*/*/*/*/\n\n"
     help+="**command:**\n!.\n**descriptin:**\nget string and send strrev\n\n/*/*/*/*/*/*/*/*/*/*/*/*/\n\n"
